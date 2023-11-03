149e5afd7c2a8c90a4b9c7d3bb6f7994554ce102
stysan <55314369+stysan@users.noreply.github.com>
2023-11-03 21:47:10 +0300

    Fix stupid fucking shit

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -1,9 +1,8 @@
 @echo off
 color 1f
-chcp 65001
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Welcome to the dependencies installer.
 echo.
 echo. Please note that:
@@ -20,47 +19,47 @@ echo. Press any key to continue...
 pause >nul
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Change dir to C:\Program Files\Python311\Scripts [1/9]
 cd C:\Program Files\Python311\Scripts
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Run pip3 install pygame --pre [2/9]
 pip3 install pygame --pre
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Run pip3 install mutagen [3/9]
 pip3 install mutagen
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Run pip3 install easing-functions [4/9]
 pip3 install easing-functions
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Run pip3 install pywin32 [5/9]
 pip3 install pywin32
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Run pip3 install pyautogui [6/9]
 pip3 install pyautogui
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Run pip3 install pypresence [7/9]
 pip3 install pypresence
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Run pip3 install numpy [8/9]
 pip3 install numpy
 cls
 echo. Install GDR dependencies
-echo ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+echo ---------------------------------
 echo. Change dir to %userprofile%\Downloads [9/9]
 cd %userprofile%\Downloads
 echo. Done!
